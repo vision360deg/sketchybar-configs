b6f4d4230129b6246f8092e5e036ec34e802a3c3
Fred Pal <vision360deg@users.noreply.github.com>
2026-08-09 04:53:38 +0200

    fix: soften Apple card border

--- items/apple.lua
+++ items/apple.lua
@@ -2,7 +2,7 @@ local colors = require("colors")
 local icons = require("icons")
 local settings = require("settings")
 
--- Padding item required because of bracket
+-- Spacing from the adjacent widget
 sbar.add("item", { width = 5 })
 
 local apple = sbar.add("item", {
@@ -17,27 +17,13 @@ local apple = sbar.add("item", {
     color = colors.widget_bg1,
     height = 28,
     corner_radius = 9,
-    border_color = colors.bg2,
+    border_color = colors.bg1,
     border_width = 2,
-    image = {
-      corner_radius = 9,
-      border_color = colors.grey,
-      border_width = 1,
-    },
   },
   padding_left = 1,
   padding_right = 1,
   click_script = "$CONFIG_DIR/helpers/menus/bin/menus -s 0"
 })
 
--- Double border for apple using a single item bracket
--- sbar.add("bracket", { apple.name }, {
---   background = {
---     color = colors.transparent,
---     height = 28,
---     border_color = colors.grey,
---   }
--- })
-
--- Padding item required because of bracket
+-- Spacing from the adjacent widget
 sbar.add("item", { width = 7 })
