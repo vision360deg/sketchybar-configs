a135a1abf57b3617b61e748e2982233fd8cb303c
Fred Pal <vision360deg@users.noreply.github.com>
2026-08-09 02:07:45 +0200

    adjust bar transparency

--- bar.lua
+++ bar.lua
@@ -3,7 +3,7 @@ local colors = require("colors")
 -- Equivalent to the --bar domain
 sbar.bar({
   height = 28,
-  color = colors.bg2,
+  color = colors.with_alpha(colors.bg2, 0.8),
   padding_right = 2,
   padding_left = 2,
   margin = 4,
